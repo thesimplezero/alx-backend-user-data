8fe57c6962c2c8addcf82abe3254acd5b73e2aac
thesimplezero <hi@thesimplezero.com>
2024-02-16 01:56:39 +0300

    clean up code

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,18 +2,26 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+
+# Import necessary modules
 import os
-from api.v1.auth.auth import Auth
+from os import getenv
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS
 
+# Import API views
+from api.v1.views import app_views
 
+# Initialize Flask app
 app = Flask(__name__)
+
+# Register API blueprints
 app.register_blueprint(app_views)
+
+# Enable CORS for all routes
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# Determine authentication type based on environment variable
 auth = None
 auth_type = os.getenv("AUTH_TYPE")
 if auth_type == "auth":
@@ -27,31 +35,31 @@ elif auth_type == "session_auth":
     auth = SessionAuth()
 
 
+# Error handling for 404 - Not Found
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """Handler for 404 Not Found errors"""
     return jsonify({"error": "Not found"}), 404
 
 
+# Error handling for 401 - Unauthorized
 @app.errorhandler(401)
 def not_authorized(error) -> str:
-    """ Not authorized handler
-    """
+    """Handler for 401 Unauthorized errors"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
+# Error handling for 403 - Forbidden
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden access handler
-    """
+    """Handler for 403 Forbidden errors"""
     return jsonify({"error": "Forbidden"}), 403
 
 
+# Filter requests before processing
 @app.before_request
 def before_request():
-    """Filtering requests
-    """
+    """Filtering requests"""
     needs_auth = ['/api/v1/status/',
                   '/api/v1/unauthorized/',
                   '/api/v1/forbidden/',
@@ -68,6 +76,7 @@ def before_request():
     request.current_user = auth.current_user(request)
 
 
+# Entry point of the application
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
