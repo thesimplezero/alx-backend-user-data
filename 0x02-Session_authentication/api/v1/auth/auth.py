4a3d240223b66c3151ea034c1b4196b25385049b
thesimplezero <hi@thesimplezero.com>
2024-02-16 02:11:52 +0300

    optimize auth class

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python3
-"""API authentication
-"""
+"""API authentication module"""
+
 from flask import request
 from typing import List, TypeVar
 import os
 
 
 class Auth():
-    """Manages API authentication
-    """
+    """Class for managing API authentication"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns true if a route requires authorization,
-        false otherwise.
-        """
+        """Check if a route requires authorization"""
         if not path or not excluded_paths or not len(excluded_paths):
             return True
         if path[-1] != '/':
             path = path + '/'
         if path in excluded_paths:
             return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Checks if request contains authorization in its
-        header.
-        """
+        """Check if request contains authorization in its header"""
         if not request:
             return None
         if not request.headers.get('Authorization'):
@@ -33,13 +28,11 @@ class Auth():
         return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Current user method
-        """
+        """Get the current user"""
         return None
 
     def session_cookie(self, request=None):
-        """Return the cookie of the session
-        """
+        """Get the session cookie"""
         if not request:
             return None
         session_name = os.getenv('SESSION_NAME')
